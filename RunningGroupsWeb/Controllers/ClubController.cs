682ba0a51932ad61c8fd526c37d158027f2674f2
YehanYeshminda <81465670+YehanYeshminda@users.noreply.github.com>
2022-09-30 00:31:50 +0530

    added data fetching

--- RunningGroupsWeb/Controllers/ClubController.cs
+++ RunningGroupsWeb/Controllers/ClubController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RunningGroupsWeb.Data;
 
 namespace RunningGroupsWeb.Controllers
 {
     public class ClubController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public ClubController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var clubs = _context.Clubs.ToList(); // sending data into the view
+            return View(clubs);
         }
     }
 }
