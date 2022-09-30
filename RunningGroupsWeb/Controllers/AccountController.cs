63124acd627efe10f63f4aa201b7248afbe2d121
YehanYeshminda <81465670+YehanYeshminda@users.noreply.github.com>
2022-10-01 01:26:20 +0530

    added the login functionality

---
+++ RunningGroupsWeb/Controllers/AccountController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RunningGroupsWeb.Data;
+using RunningGroupsWeb.Models;
+using RunningGroupsWeb.ViewModel;
+
+namespace RunningGroupsWeb.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly ApplicationDbContext _context;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, ApplicationDbContext context)
+        {
+            _context = context;
+            _signInManager = signInManager;
+            _userManager = userManager;
+
+        }
+
+        public IActionResult Login()
+        {
+            var responce = new LoginViewModel();
+            return View(responce);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            if (!ModelState.IsValid) return View(loginViewModel);
+
+            var user = await _userManager.FindByEmailAsync(loginViewModel.EmailAddress);
+
+            if (user != null)
+            {
+                // user is present
+                var passwordCheck = await _userManager.CheckPasswordAsync(user, loginViewModel.Password);
+
+                if (passwordCheck)
+                {
+                    // password is correct
+                    var result = await _signInManager.PasswordSignInAsync(user, loginViewModel.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        // if the password and the email is correct
+                        return RedirectToAction("Index", "Race");
+                    }
+                }
+
+                // if password is wrong 
+                TempData["Error"] = "Wrong Credentials! Try Again!";
+                return View(loginViewModel);
+            }
+            // user not found
+            TempData["Error"] = "Wrong credentials! Try Again!";
+            return View(loginViewModel);
+        }
+    }
+}
